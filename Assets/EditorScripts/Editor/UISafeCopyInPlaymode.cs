0111e3657acd0276fd3a37fd4c087380f1625321
rufreakde <rc@dev-six.com>
2019-12-21 23:58:10 +0100

    changing project settings with UVR pipeline

---
+++ Assets/EditorScripts/Editor/UISafeCopyInPlaymode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(Canvas))]
+public class UISafeCopyInPlaymode : Editor 
+{
+    //Note this script will override the prefab if it exists already.
+    string uiPath = "Assets/";
+    string cloneName = "will be set automaticaly";
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector ();
+
+        if(GUILayout.Button("Save UI during Playmode!")){
+            cloneTheUI();
+        }
+    }
+
+    void cloneTheUI(){
+        Canvas myTarget = (Canvas)target;
+        cloneName = myTarget.transform.name+".prefab";
+        PrefabUtility.SaveAsPrefabAsset(myTarget.gameObject, uiPath + cloneName);
+        Debug.Log("Saved here: " + uiPath+cloneName);
+    }
+}
