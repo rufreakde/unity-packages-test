6190b055bf87ffa1db80d1785329674bb9d0a8bb
rufreakde <rc@dev-six.com>
2020-07-15 18:00:56 +0200

    work in progress breaks things

---
+++ Assets/testScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    [InfoBox("TODO: -Fix Manager System so it can be used smoothly on new setups!")]
+    [Slider(0.1f,2.0f)]
+    [SerializeField]
+    private float scaleOfSprite = 1.0f;
+
+    public float ScaleOfSprite
+    {
+        get {
+            return scaleOfSprite;
+        }
+        set {
+            scaleOfSprite = value;
+            this.transform.localScale = new Vector3(value, value, value);
+        }
+    }
+   
+}
